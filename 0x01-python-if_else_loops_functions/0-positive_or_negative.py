bfd2bbc6519d6d7a45ee9d59395af8f931be673c
Quammy93 <Quammy93@gmail.com>
2023-01-05 00:50:39 -0800

    source code

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import random
-randomnumber = random.randint(98, 98)
-print(randomnumber)
-randomnumber = random.randint(-98, -98)
-print(randomnumber)
+randomnumber = random.randint(-10, 10)
+rand_list = []   
+for i in range(0,5):
+	 n = random.randint(-10,10)
+	rand_list.append(n)
+	print(rand_list)
