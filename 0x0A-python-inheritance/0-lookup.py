55c5e197083fac7e24ab391a19a30186cfc2209a
Quammy93 <Quammy93@gmail.com>
2023-02-07 04:38:04 -0800

    inheritanc

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def lookup(obj):
-	lists = []
-for list in range(obj, obj ** 2 + 1) :
-	lists.append(list)
-	return lists
+    lists = []
+
+
+for list in range(obj, obj ** 2 + 1):
+    lists.append(list)
+    return lists
