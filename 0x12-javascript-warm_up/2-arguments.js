86df2ee13091dcf0bbba53ea898d1acbaeb894f0
Quammy93 <Quammy93@gmail.com>
2023-04-12 13:37:19 -0700

    javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.arg.lenght === 2) {
+  console.log('No Argument');
+} else if (process.arg.lenght === 3) {
+  console.log('Argument Found');
+} else {
+  console.log('Argument found');
+}
