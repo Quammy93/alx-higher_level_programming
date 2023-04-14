11c29915a1ac8f88b2124ed4ffb649c2d9e89cd5
Quammy93 <Quammy93@gmail.com>
2023-04-13 23:31:05 -0700

    class object

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/Node
+class Rectangle {
+  constrctor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/Node
+class Rectangle {
+  constractor (w, h) {
+   if ((w > 0) && (h > 0)) {
+   this.width = 0;
+   this.height = 0
+   print(): console.log(X)
+   }
+  }
+}
+
+module.exports = Rectangle;
