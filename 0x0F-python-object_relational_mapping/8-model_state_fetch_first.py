dad6b4da3e6e6f632a5f1aa0306e499e4b09ca9a
Quammy93 <Quammy93@gmail.com>
2023-04-16 16:30:06 -0700

    database

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,6 +13,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')):
+    instance = session.query(State).first()
+    if instance is None:
+        print("Nothing")
+    else:
         print(instance.id, instance.name, sep=": ")
-
