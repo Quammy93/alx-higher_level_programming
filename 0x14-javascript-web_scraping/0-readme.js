86af1c1c96705c8c9c8d27908299d1d3f33f0d0b
Quammy93 <quammy93@gmail.com>
2023-05-25 01:43:09 +0000

    javascript

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,4 @@
-#!/usr/bin/node
 const fs = require('fs');
-
-//const filePath = process.argv[2];
-
-fs.readFile(process.argv[2], 'utf-8', function(error, content) {
-    console.error(`An error occurred while reading the file: ${error}`);
-    console.log(content);
-  }
-);
-
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
